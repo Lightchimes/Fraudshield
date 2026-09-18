d138c09d5cf7bba22424ba8abb576c3dcfd34f90
Lightchimes <lightchimes@gmail.com>
2026-09-18 14:18:01 +0100

    Remove honeypot check from scoring logic
    Removed honeypot check and associated score adjustment.

--- workers.js
+++ workers.js
@@ -1351,13 +1351,7 @@ if (data.leaked === true) {
     "IPQS reports that this phone number has appeared in a recent data breach or compromise."
   );
 }
-if (data.honeypot === true) {
-  score += 25;
-
-  signals.push(
-    "IPQS identifies this phone number as a potential honeypot or trap number."
-  );
-}        
+      
 
 if (data.active === false) {
           score += 15;
